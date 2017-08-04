ecd67eef68019f14bd676dec853ec42173774825
zmarci <z.marton13@gmail.com>
2017-08-04 08:12:57 +0200

    basic project

---
+++ Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Marton Zatrok on 2017. 08. 04..
+//  Copyright © 2017. Marton Zatrok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
