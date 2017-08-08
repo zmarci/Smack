f953394a3be96d25644d7a71c4263bab7dea6562
zmarci <z.marton13@gmail.com>
2017-08-08 08:54:51 +0200

    Added login view with constraints and button to dismiss it

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Marton Zatrok on 2017. 08. 08..
+//  Copyright © 2017. Marton Zatrok. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Marton Zatrok on 2017. 08. 08..
+//  Copyright © 2017. Marton Zatrok. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
